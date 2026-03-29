46a8e8d63be321f4c0ffa1a3c0e48ff2055e778f
Mani Setayesh <102564262+manisetayesh@users.noreply.github.com>
2026-03-28 23:19:56 -0400

    added tooltips to scatterplot and formula

--- data/linear.py
+++ data/linear.py
@@ -23,18 +23,14 @@ y = df[target]
 model = LinearRegression()
 model.fit(X, y)
 
-# 3. Get Feature Importance
-# This tells you which musical attributes actually drive popularity
-
-# 4. Generate Predictions
 df["predicted"] = model.predict(X)
-output_data = df[["Popularity", "predicted"]].to_dict(orient="records")
+output_data = df.to_dict(orient="records")
 with open("data/predictions.json", "w") as f:
     json.dump({"data": output_data}, f)
 
-print(f"Model Accuracy (R^2): {model.score(X, y):.4f}")
-coefficients = dict(zip(features, model.coef_.tolist()))
-intercept = model.intercept_
+# print(f"Model Accuracy (R^2): {model.score(X, y):.4f}")
+# coefficients = dict(zip(features, model.coef_.tolist()))
+# intercept = model.intercept_
 
-print(f"Intercept (Beta 0): {intercept}")
-print("Coefficients:", json.dumps(coefficients, indent=4))
+# print(f"Intercept (Beta 0): {intercept}")
+# print("Coefficients:", json.dumps(coefficients, indent=4))
